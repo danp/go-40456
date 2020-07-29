7bdb0ef9a3c9ee539b4c57fe632fded1967226d4
Dan Peterson <dpiddy@gmail.com>
2020-07-29 09:03:59 -0300

    wip

--- main.go
+++ main.go
@@ -1,11 +1,13 @@
 package main
 
 import (
+	"context"
+
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	typev1 "k8s.io/client-go/kubernetes/typed/core/v1"
 )
 
 func main() {
 	var x typev1.EndpointsInterface
-	x.List(metav1.ListOptions{})
+	x.List(context.Background(), metav1.ListOptions{})
 }
